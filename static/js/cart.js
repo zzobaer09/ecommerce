599f0634848fabc58d11b99e6f2ba4d34de94498
Zobaer <zzobaer09@gmail.com>
2022-11-02 21:34:19 +0600

    done

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,3 +1,35 @@
-console.log("hello world")
+const allbtn = document.getElementsByClassName("update-btn")
 
-console.log(user);
+for (let item of allbtn){
+	item.addEventListener("click" , function() {
+		if (user == "AnonymousUser"){
+			console.log("log in");
+		}else{
+			const productId = this.dataset.productid
+			const action = this.dataset.action
+			updateCart(productId , action)
+		}
+	})
+}
+
+
+
+
+function updateCart(productId , action) {
+	console.log("sending data")
+	const url = "/update_store/"
+	fetch(url , {
+		method: 'POST',
+		headers: {
+			"Content-Type": "application/json",
+			"X-CSRFToken": csrftoken,
+		},
+		body:JSON.stringify({'productId':productId, 'action':action})
+	})
+	.then((request)=>{
+		return request.json()
+	})
+	.then((data)=>{
+		console.log(data);
+	})
+}
--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path("" , views.store , name="store"),
     path("cart/" , views.cart , name="cart"),
     path("checkout/", views.checkout , name="checkout"),
+
+    path("update_store/" , views.updateStore , name="updateStore"),
 ]
